fcb5b02416b72f0362c153b11497d3af30bb85f4
Meowizz <meowizz17@gmail.com>
2026-07-14 16:28:41 +0300

    Update os env for server

--- cmd/server/flag.go
+++ cmd/server/flag.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
+
+	"github.com/caarlos0/env/v6"
 )
 
 type ConfigAddr struct {
-	Addr string
+	Addr string `env:"ADDRESS" envDefault:"localhost:8080"`
 }
 
 func ParseFlag() *ConfigAddr {
-	cfg := &ConfigAddr{
-		Addr: "localhost:8080",
-	}
-	flag.StringVar(&cfg.Addr, "a", cfg.Addr, "Net address host:port")
+	var flagAddr string
+	cfg := &ConfigAddr{}
+	err := env.Parse(cfg)
 
+	if err != nil {
+		log.Fatal("Error parsing server argumnet for start")
+	}
+
+	flag.StringVar(&flagAddr, "a", "", "Net address host:port")
 	flag.Parse()
+
+	if os.Getenv("ADDRESS") == "" && flagAddr != "" {
+		cfg.Addr = flagAddr
+	}
+
 	return cfg
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Meowizz/metrics-collector
 
 go 1.25.2
 
-require github.com/go-chi/chi/v5 v5.3.0
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-chi/chi/v5 v5.3.0
+)
